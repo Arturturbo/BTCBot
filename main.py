1b32f350ff5c3ec9a90f4a4b1066b40498abddab
Arturturbo <arturlihovither@gmail.com>
2025-05-20 14:10:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,10 +63,18 @@ async def send_btc_report():
         bot = Bot(token=TOKEN)
         await bot.send_message(chat_id=CHAT_ID, text=message)
 
-    except Exception as e:
-        print(f"Ошибка: {e}")
+   import asyncio
+from telegram import Bot
+
+async def main():
+    token = "7889272376:AAHedHwphIgd8lbLIGscYrAf9KIZ04MsXlc"
+    chat_id = "5034238573"
+    bot = Bot(token=token)
+    await bot.send_message(chat_id=chat_id, text="✅ Бот с Render работает!")
+
 if __name__ == '__main__':
     asyncio.run(main())
+
 
 
 
